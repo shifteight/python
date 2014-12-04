1b8d0610626796169dd934b6ff7052fa5d076e48
Kevin Qian <yanxiang.qian@gmail.com>
2014-12-04 22:11:26 +0800

    chapter 4

--- DSA/english_ruler.py
+++ DSA/english_ruler.py
@@ -5,12 +5,14 @@ def draw_line(tick_length, tick_label=''):
         line += ' ' + tick_label
     print(line)
 
+
 def draw_interval(center_length):
     """Draw tick interval based upon a central tick length."""
     if center_length > 0:
         draw_interval(center_length - 1)
         draw_line(center_length)
         draw_interval(center_length - 1)
+
 
 def draw_ruler(num_inches, major_length):
     """Draw English ruler with given number of inches, major tick length."""
