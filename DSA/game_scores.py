d303d3bc1f126216de9cf539951ec5cfe44f38a8
Kevin Qian <yanxiang.qian@gmail.com>
2014-12-13 22:12:28 +0800

    chapter 6

---
+++ DSA/game_scores.py
@@ -0,0 +1,55 @@
+__author__ = 'kevin'
+
+
+class GameEntry:
+    """Represents one entry of a list of high scores."""
+
+    def __init__(self, name, score):
+        self._name = name
+        self._score = score
+
+    def get_name(self):
+        return self._name
+
+    def get_score(self):
+        return self._score
+
+    def __str__(self):
+        return '({0}, {1}'.format(self._name, self._score)
+
+
+class Scoreboard:
+    """Fixed-length sequence of high scores in non-decreasing order."""
+
+    def __init__(self, capacity=10):
+        """Initialize scoreboard with given maximum capacity.
+
+        All entries are initially None.
+        """
+        self._board = [None] * capacity
+        self._n = 0
+
+    def __getitem__(self, k):
+        """Return entry at index k."""
+        return self._board[k]
+
+    def __str__(self):
+        """Return string representation of the high score list."""
+        return '\n'.join(str(self._board[j]) for j in range(self._n))
+
+    def add(self, entry):
+        """Consider adding entry to high scores."""
+        score = entry.get_score()
+
+        good = self._n < len(self._board) or score > self._board[-1].get_score()
+
+        if good:
+            if self._n < len(self._board):  # no score drops from list
+                self._n += 1
+
+            # shift lower scores rightward to make room for new entry
+            j = self._n - 1
+            while j > 0 and self._board[j-1].get_score() < score:
+                self._board[j] = self._board[j-1]
+                j -= 1
+            self._board[j] = entry
---
+++ DSA/stacks.py
@@ -0,0 +1,78 @@
+__author__ = 'kevin'
+
+
+class Empty(Exception):
+    """Error attempting to access an element from an empty container."""
+    pass
+
+
+class ArrayStack:
+    """LIFO Stack implementation using a Python list as underlying storage."""
+
+    def __init__(self):
+        """Create an empty stack."""
+        self._data = []
+
+    def __len__(self):
+        """Return the number of elements in the stack."""
+        return len(self._data)
+
+    def is_empty(self):
+        """Return True if the stack is empty."""
+        return len(self._data) == 0
+
+    def push(self, e):
+        """Add element e to the top of the stack."""
+        self._data.append(e)
+
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack.
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]
+
+    def pop(self):
+        """Remove and return the element from the top of the stack (i.e., LIFO).
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()
+
+
+# Some applications
+
+
+def reverse_file(filename):
+    """Overwrite given file with its contents line-by-line reversed."""
+    S = ArrayStack()
+    original = open(filename)
+    for line in original:
+        S.push(line.rstrip('\n'))
+    original.close()
+
+    # now we overwrite with contents in LIFO order
+    output = open(filename, 'w')
+    while not S.is_empty():
+        output.write(S.pop() + '\n')
+    output.close()
+
+
+def is_matched(expr):
+    """Return True if all delimiters are properly match."""
+    lefty = '({['
+    righty = ')}]'
+    S = ArrayStack()
+    for c in expr:
+        if c in lefty:
+            S.push(c)
+        elif c in righty:
+            if S.is_empty():
+                return False
+            if righty.index(c) != lefty.index(S.pop()):
+                return False
+    return S.is_empty()
