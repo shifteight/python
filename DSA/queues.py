00f6660d92ab8b1226378be7b4a0fd3bcd134e32
Kevin Qian <yanxiang.qian@gmail.com>
2014-12-15 18:56:47 +0800

    queue

--- DSA/queues.py
+++ DSA/queues.py
@@ -1,5 +1,6 @@
 __author__ = 'kevin'
 
+from stacks import Empty
 
 class ArrayQueue:
     """FIFO queue implementation using a Python list as underlying storage."""
@@ -19,3 +20,42 @@ class ArrayQueue:
         """Return True if the queue is empty."""
         return self._size == 0
 
+    def first(self):
+        """Return (but do not remove) the element at the front of the queue.
+
+        Raise Empty exception if the queue is empty.
+        """
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        return self._data[self._front]
+
+    def dequeue(self):
+        """Remove and return the first element of the queue (i.e., FIFO).
+
+        Raise Empty exception if the queue is empty.
+        """
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        answer = self._data[self._front]
+        self._data[self._front] = None
+        self._front = (self._front + 1) % len(self._data)
+        self._size -= 1
+        return answer
+
+    def enqueue(self, e):
+        """Add an element to the back of the queue."""
+        if self._size == len(self._data):
+            self._resize(2 * len(self._data))
+        avail = (self._front + self._size) % len(self._data)
+        self._data[avail] = e
+        self._size += 1
+
+    def _resize(self, cap):
+        """Resize to a new list of capacity >= len(self)."""
+        old = self._data
+        self._data = [None] * cap
+        walk = self._front
+        for k in range(self._size):
+            self._data[k] = old[walk]
+            walk = (1 + walk) % len(old)
+        self._front = 0
