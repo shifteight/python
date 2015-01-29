ab64305c073c1dc5bbcae44ea1169ebe33ead2f1
Kevin Qian <yanxiang.qian@gmail.com>
2015-01-29 10:17:01 +0800

    wumpus 2

--- hellopython/classes/wumpus2/monster.py
+++ hellopython/classes/wumpus2/monster.py
@@ -9,6 +9,12 @@ class Monster(player.Player):
         self.description = description
 
     def get_input(self):
+        if not self.playing:
+            return ""
+        player_present = [x for x in self.location.here
+                          if x.name == "Player"]
+        if player_present:
+            return "attack " + player_present[0].name
         if random.choice((0, 1)):
             return ("go " + random.choice(self.location.exits()))
         else:
--- hellopython/classes/wumpus2/player.py
+++ hellopython/classes/wumpus2/player.py
@@ -4,6 +4,10 @@ import shlex
 class Player(object):
 
     def __init__(self, location):
+        self.name = "Player"
+        self.description = "The Player"
+        self.hit_points = 3
+        self.events = []
         self.location = location
         self.location.here.append(self)
         self.playing = True
@@ -61,9 +65,38 @@ class Player(object):
         else:
             result += ["nothing!"]
         return result
-    
 
+    def update(self):
+        self.result = self.process_input(self.get_input())
 
+    def attach(self, player, noun):
+        if player == self:
+            return ["You can't attack yourself!"]
+        hit_chance = 2
+        has_sword = [i for i in player.inventory
+                     if i.name == 'sword']
+        if has_sword:
+            hit_chance += 2
+
+        rool = random.choice([1,2,3,4,5,6])
+        if roll > hit_chance:
+            self.events.append("The " + player.name + " misses you!")
+            return ["You miss the " + self.name]
+
+        self.hit_points -= 1
+        if self.hit_points <= 0:
+            return_value = ["You kill the " + self.name]
+            self.events.append("The " + player.name + " has killed you!")
+            self.die()
+            return return_value
+
+        self.events.append("The " + player.name + " hits you!")
+        return ["You hit the " + self.name]
+
+    def die(self):
+        self.playing = False
+        self.input = ""
+        self.name = "A dead " + self.name
 if __name__ == '__main__':
     import cave
     caves = cave.create_caves()
@@ -82,4 +115,5 @@ if __name__ == '__main__':
     while player.playing:
         input = player.get_input()
         result = player.process_input(input)
+        player.update()
         print "\n".join(result)
