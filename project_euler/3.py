d4027d561e3e95cc1cb55d7f58dc0e3004b6d59d
Kevin Qian <scifi.org@gmail.com>
2016-04-20 23:07:11 +0800

    euler 3

--- project_euler/3.py
+++ project_euler/3.py
@@ -4,3 +4,7 @@
 # 
 # What is the largest prime factor of the number 600851475143 ?
 
+def factors(n):
+	# find a set of all factors of possibly VERY large integer n
+	return set(reduce(list.__add__, 
+            ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
