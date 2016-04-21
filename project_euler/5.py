81de2355cb92771deca965f073e279680f9e8fd0
Kevin Qian <scifi.org@gmail.com>
2016-04-21 19:33:43 +0800

    euler 5

--- project_euler/5.py
+++ project_euler/5.py
@@ -5,17 +5,16 @@
 # What is the smallest positive number that is evenly divisible by all
 # of the numbers from 1 to 20?
 
-def is_ed_1_10(n):
-    for j in range(1, 11):
+def all_divisible(n, m):
+    for j in range(1, m+1):
         if n % j != 0:
-            return False
-        if n // j % 2 == 1:
             return False
     return True
 
-n = 21
+n = 2520
+m = 20
 while True:
-    if is_ed_1_20(n):
+    if all_divisible(n, m):
         break
     n += 1
 
