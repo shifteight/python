cfcb1b6020a721a2b065f18067e05781e2c87111
Kevin Qian <scifi.org@gmail.com>
2016-12-05 15:20:21 +0800

    ch2 vowels

--- headfirst/ch2/vowels.py
+++ headfirst/ch2/vowels.py
@@ -1,10 +1,13 @@
 
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = input("Provide a word to search for vowels: ")
-found = []
+
+found = {}
+
 for letter in word:
     if letter in vowels:
-        if letter not in found:
-            found.append(letter)
-for vowel in found:
-    print(vowel)
+        found.setdefault(letter, 0)
+        found[letter] += 1
+
+for k, v in sorted(found.items()):
+    print(k, 'was found', v, 'time(s).')
