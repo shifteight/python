f06c4146ded5b03d12f4576d9c6474bc17c926a6
Kevin Qian <scifi.org@gmail.com>
2017-12-13 16:56:20 +0800

    liaoxuefeng, primes

---
+++ liaoxuefeng/primes.py
@@ -0,0 +1,29 @@
+
+
+# An odd-number generator (staring from 3)
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_devisible(n):
+    return lambda x: x % n > 0
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_devisible(n), it)
+
+def main():
+    for n in primes():
+        if n < 1000:
+            print(n)
+        else:
+            break
+
+if __name__ == '__main__':
+    main()
