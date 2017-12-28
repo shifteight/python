c8677023b7ee51fb2d4c33127fd5a976c4b6c429
Kevin Qian <scifi.org@gmail.com>
2017-12-28 22:24:49 +0800

    101, config parser

--- python101/config_parser.py
+++ python101/config_parser.py
@@ -1,4 +1,6 @@
 import configparser
+import os
+
 
 def createConfig(path):
     """
@@ -16,6 +18,69 @@ def createConfig(path):
         config.write(config_file)
 
 
+def get_config(path):
+    """
+    Returns the config object
+    """
+    if not os.path.exists(path):
+        createConfig(path)
+
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+
+def get_setting(path, section, setting):
+    """
+    Print out a setting
+    """
+    config = get_config(path)
+    value = config.get(section, setting)
+    msg = "{section} {setting} is {value}".format(
+        section=section, setting=setting, value=value
+    )
+    print(msg)
+    return value
+
+
+def update_setting(path, section, setting, value):
+    """
+    Update a setting
+    """
+    config = get_config(path)
+    config.set(section, setting, value)
+    with open(path, 'w') as config_file:
+        config.write(config_file)
+
+
+def delete_setting(path, section, setting):
+    """
+    Delete a setting
+    """
+    config = get_config(path)
+    config.remove_option(section, setting)
+    with open(path, 'w') as config_file:
+        config.write(config_file)
+
+
+def interpolationDemo(path):
+    if not os.path.exists(path):
+        createConfig(path)
+
+    config = configparser.ConfigParser()
+    config.read(path)
+
+    print(config.get("Settings", "font_info"))
+    print(config.get("Settings", "font_info",
+                     vars={"font": "Arial", "font_size": "100"}))
+
+
 if __name__ == "__main__":
     path = "settings.ini"
-    createConfig(path)
+    font = get_setting(path, 'Settings', 'font')
+    font_size = get_setting(path, 'Settings', 'font_size')
+
+    update_setting(path, 'Settings', 'font_size', '12')
+    delete_setting(path, 'Settings', 'font_style')
+
+    interpolationDemo(path)
