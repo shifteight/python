8b0e20b044757aba3dab03ca9b7b7ed49838602b
Kevin Qian <scifi.org@gmail.com>
2018-05-16 16:49:04 +0800

    PSADS, 1.13

--- PSADS/fraction.py
+++ PSADS/fraction.py
@@ -15,11 +15,13 @@ class Fraction:
         self.num = top
         self.den = bottom
 
-    def show(self):
-        print(self.num, '/', self.den)
-
     def __str__(self):
-        return str(self.num) + '/' + str(self.den)
+        if self.num == 0:
+            return str(0)
+        elif self.den == 1:
+            return str(self.num)
+        else:
+            return str(self.num) + '/' + str(self.den)
 
     def __add__(self, otherFraction):
 
@@ -28,16 +30,35 @@ class Fraction:
         common = gcd(newNum, newDen)
         return Fraction(newNum // common, newDen // common)
 
+    def __sub__(self, otherFraction):
+        newNum = self.num * otherFraction.den - self.den * otherFraction.num
+        newDen = self.den * otherFraction.den
+        common = gcd(newNum, newDen)
+        return Fraction(newNum // common, newDen // common)
+
+    def __mul__(self, otherFraction):
+        newNum = self.num * otherFraction.num
+        newDen = self.den * otherFraction.den
+        common = gcd(newNum, newDen)
+        return Fraction(newNum // common, newDen // common)
+
+    def __truediv__(self, otherFraction):
+        if otherFraction.num == 0:
+            raise ValueError
+        else:
+            return self.__mul__(Fraction(otherFraction.den, otherFraction.num))
+
     def __eq__(self, other):
         return self.num * other.den == other.num * self.den
 
 
-f1 = Fraction(1, 4)
-f2 = Fraction(1, 4)
-f3 = f1 + f2
-print(f3)
+if __name__ == '__main__':
+    f1 = Fraction(4, 8)
+    f2 = Fraction(1, 8)
+    f3 = f1 / f2
+    print(f3)
 
-if f1 == f2:
-    print("equal")
-else:
-    print("not equal")
+    if f1 == f2:
+        print("equal")
+    else:
+        print("not equal")
--- PSADS/logic_gate.py
+++ PSADS/logic_gate.py
@@ -1,11 +1,11 @@
 class LogicGate:
 
     def __init__(self, n):
-        self.label = n
+        self.name = n
         self.output = None
 
-    def getLabel(self):
-        return self.label
+    def getName(self):
+        return self.name
 
     def getOutput(self):
         self.output = self.performGateLogic()
@@ -21,10 +21,25 @@ class BinaryGate(LogicGate):
         self.pinB = None
 
     def getPinA(self):
-        return int(input("Enter Pin A input for gate " + self.getLabel() + '-->'))
+        if self.pinA == None:
+            return int(input("Enter Pin A input for gate " + self.getName() + '-->'))
+        else:
+            return self.pinA.getFrom().getOutput()
 
     def getPinB(self):
-        return int(input("Enter Pin B input for gate " + self.getLabel() + '-->'))
+        if self.pinB == None:
+            return int(input("Enter Pin B input for gate " + self.getName() + '-->'))
+        else:
+            return self.pinB.getFrom().getOutput()
+
+    def setNextPin(self, source):
+        if self.pinA == None:
+            self.pinA = source
+        else:
+            if self.pinB == None:
+                self.pinB = source
+            else:
+                print("Cannot Connect: NO EMPTY PINS on this gate")
 
 
 class UnaryGate(LogicGate):
@@ -35,4 +50,84 @@ class UnaryGate(LogicGate):
         self.pin = None
 
     def getPin(self):
-        return int(input("Enter Pin input for gate " + self.getLabel() + '-->'))
+        if self.pin == None:
+            return int(input("Enter Pin input for gate " + self.getName() + '-->'))
+        else:
+            return self.pin.getFrom().getOutput()
+
+    def setNextPin(self, source):
+        if self.pin == None:
+            self.pin = source
+        else:
+            print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class AndGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+
+class OrGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 0 and b == 0:
+            return 0
+        else:
+            return 1
+
+
+class NotGate(UnaryGate):
+
+    def __init__(self, n):
+        UnaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+
+        if self.getPin():
+            return 0
+        else:
+            return 1
+
+
+class Connector:
+
+    def __init__(self, fgate, tgate):
+        self.fromgate = fgate
+        self.togate = tgate
+
+        tgate.setNextPin(self)
+
+    def getFrom(self):
+        return self.fromgate
+
+    def getTo(self):
+        return self.togate
+
+
+
+# MAIN PROGRAM
+if __name__ == '__main__':
+    g1 = AndGate("G1")
+    g2 = AndGate("G2")
+    g3 = OrGate("G3")
+    g4 = NotGate("G4")
+    c1 = Connector(g1, g3)
+    c2 = Connector(g2, g3)
+    c3 = Connector(g3, g4)
+    print(g4.getOutput())
