41037ed522582284c2eec9285da2df741e0e7c92
Kevin Qian <yanxiang.qian@gmail.com>
2019-05-12 23:24:36 +0800

    add python 100day

---
+++ 100day/narcissistic.py
@@ -0,0 +1,22 @@
+def narcissistic_number_1(num):
+    
+    length = len(str(num))
+     
+    count = length    
+    
+    num_sum = 0    
+     
+    while count:        
+        
+        num_sum += ((num // 10 ** (count - 1)) % 10) ** length     
+         
+        count -= 1    
+     
+    else:       
+        
+        return num_sum == num
+
+for i in range(10**6):
+    if narcissistic_number_1(i):
+        print("%d is %d bit narcissistic_number" % (i, len(str(i))))
+    
