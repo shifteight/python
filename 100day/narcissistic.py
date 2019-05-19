8dff77959d2d64ea1d94e3f164e335dd3fcb53e7
Kevin Qian <yanxiang.qian@gmail.com>
2019-05-19 17:02:23 +0800

    format a file using autopep8

--- 100day/narcissistic.py
+++ 100day/narcissistic.py
@@ -1,22 +1,14 @@
 def narcissistic_number_1(num):
-    
     length = len(str(num))
-     
-    count = length    
-    
-    num_sum = 0    
-     
-    while count:        
-        
-        num_sum += ((num // 10 ** (count - 1)) % 10) ** length     
-         
-        count -= 1    
-     
-    else:       
-        
+    count = length
+    num_sum = 0
+    while count:
+        num_sum += ((num // 10 ** (count - 1)) % 10) ** length
+        count -= 1
+    else:
         return num_sum == num
+
 
 for i in range(10**6):
     if narcissistic_number_1(i):
         print("%d is %d bit narcissistic_number" % (i, len(str(i))))
-    
