09a43b3113467ce4eb65218076d1225f3f688af2
Kevin Qian <yanxiang.qian@gmail.com>
2019-06-03 21:32:00 +0800

    crash course

--- crashcourse/file_reader.py
+++ crashcourse/file_reader.py
@@ -1,3 +1,5 @@
 with open('pi_digits.txt') as file_object:
-    for line in file_object:
-        print(line.rstrip())
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
