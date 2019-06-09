ee0a0888cbc98306b25b78e03834fb90b2ed05b8
Kevin Qian <yanxiang.qian@gmail.com>
2019-06-09 22:15:35 +0800

    crash course - alien

--- crashcourse/alien/settings.py
+++ crashcourse/alien/settings.py
@@ -1,6 +1,7 @@
 class Settings():
 
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_width = 400
+        self.screen_height = 300
+        self.bg_color = (0, 0, 255)
+        self.ship_speed_factor = 1.5
